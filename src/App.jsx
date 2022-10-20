6726051941c39677faff6bcf0d01d3793ce0e2e4
jabenjam <jabenjam@student.42.fr>
2022-10-20 21:38:17 +0200

    started from scratch

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import React from 'react'
+import About from './components/About'
+import Contact from './components/Contact'
+import Education from './components/Education'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Projects from './components/Projects'
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        WIP PORTFOLIO APP
+      </h1>
+      <Header />
+      <About />
+      <Education />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Education/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Education.css'
+
+const Education = () => {
+  return (
+    <div>
+      <h1>
+        Education
+      </h1>
+      <img class="badge" src="https://badge42.vercel.app/api/v2/cl9h8a93b00390hmqa9bg4a7c/stats?cursusId=21&coalitionId=48" alt="" />
+    </div>
+  )
+}
+
+export default Education
