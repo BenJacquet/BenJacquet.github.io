b74965da1bc8c1f4b9108ed4b2af9afc2beb3a9b
jabenjam <jabenjam@student.42.fr>
2022-10-23 22:47:13 +0200

    fixed height of skill component and removed wip sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Header from './components/Header'
 import Navbar from './components/Navbar'
 import About from './components/About'
 import Skills from './components/Skills'
-import Education from './components/Education'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+// import Education from './components/Education'
+// import Projects from './components/Projects'
+// import Contact from './components/Contact'
+// import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -16,10 +16,10 @@ const App = () => {
       <Navbar/>
       <About />
       <Skills />
-      <Education />
-      <Projects />
-      <Contact />
-      <Footer />
+      {/* <Education /> */}
+      {/* <Projects /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
     </div>
   )
 }
--- src/components/CallToAction/index.jsx
+++ src/components/CallToAction/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './CallToAction.css'
-// import ResumeEN from '../../assets/pdfs/Benjamin Jacquet - Resume.pdf'
-import ResumeFR from '../../assets/pdfs/Benjamin Jacquet - CV.pdf'
+import ResumeEN from '../../assets/pdfs/Benjamin Jacquet - CV - EN.pdf'
+import ResumeFR from '../../assets/pdfs/Benjamin Jacquet - CV - FR.pdf'
 
 const CallToAction = () => {
   return (
     <div className='cta'>
-      <a href={ResumeFR} className='btn'>Download Resume</a>
+      <a href={ResumeFR} className='btn'>CV - FR</a>
+      <a href={ResumeEN} className='btn'>CV - EN</a>
       <a href="#contact" className='btn btn-primary' >Want to Talk ?</a>
     </div>
   )
