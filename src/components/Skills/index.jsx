5316881167b5cff3da5f81375245fcc1477dafce
jabenjam <jabenjam@student.42.fr>
2022-10-26 18:29:11 +0200

    update des cv et mise à jour de l'onglet skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -9,14 +9,47 @@ const Skills = () => {
       <div className='container'>
         <div className="skills__container">
           <div className="category">
-            <h3>Frontend</h3>
+            <h3>Frameworks</h3>
             <div className="skills__content">
-              <article className="skill__details">
-                <h4>HTML5</h4>
+            <article className="skill__details">
+                <h4>Vuetify</h4>
                 <small className='text-light'>Experienced</small>
               </article>
               <article className="skill__details">
-                <h4>CSS3</h4>
+                <h4>Vue</h4>
+                <small className='text-light'>Intermediate</small>
+              </article>
+              <article className="skill__details">
+                <h4>Nuxt</h4>
+                <small className='text-light'>Intermediate</small>
+              </article>
+              <article className="skill__details">
+                <h4>Colyseus</h4>
+                <small className='text-light'>Intermediate</small>
+              </article>
+              <article className="skill__details">
+                <h4>React</h4>
+                <small className='text-light'>Beginner</small>
+              </article>
+            </div>
+          </div>
+          <div className="category">
+            <h3>Languages</h3>
+            <div className="skills__content">
+              <article className="skill__details">
+                <h4>C & C++</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+              <article className="skill__details">
+                <h4>Bash & Shell</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+              <article className="skill__details">
+                <h4>Makefile</h4>
+                <small className='text-light'>Experienced</small>
+              </article>
+              <article className="skill__details">
+                <h4>HTML5 & CSS3</h4>
                 <small className='text-light'>Experienced</small>
               </article>
               <article className="skill__details">
@@ -24,16 +57,8 @@ const Skills = () => {
                 <small className='text-light'>Intermediate</small>
               </article>
               <article className="skill__details">
-                <h4>Vue & Nuxt</h4>
-                <small className='text-light'>Experienced</small>
-              </article>
-              <article className="skill__details">
-                <h4>Vuetify</h4>
-                <small className='text-light'>Experienced</small>
-              </article>
-              <article className="skill__details">
-                <h4>React</h4>
-                <small className='text-light'>Intermediate</small>
+                <h4>ASMx64</h4>
+                <small className='text-light'>Beginner</small>
               </article>
             </div>
           </div>
@@ -45,12 +70,8 @@ const Skills = () => {
                 <small className='text-light'>Experienced</small>
               </article>
               <article className="skill__details">
-                <h4>Docker & Docker-Compose</h4>
+                <h4>Docker</h4>
                 <small className='text-light'>Experienced</small>
-              </article>
-              <article className="skill__details">
-                <h4>Kubernetes</h4>
-                <small className='text-light'>Beginner</small>
               </article>
               <article className="skill__details">
                 <h4>Nginx</h4>
@@ -60,30 +81,9 @@ const Skills = () => {
                 <h4>Node.Js</h4>
                 <small className='text-light'>Intermediate</small>
               </article>
-            </div>
-          </div>
-          <div className="category">
-            <h3>Wizardry</h3>
-            <div className="skills__content">
               <article className="skill__details">
-                <h4>C</h4>
-                <small className='text-light'>Experienced</small>
-              </article>
-              <article className="skill__details">
-                <h4>C++</h4>
-                <small className='text-light'>Experienced</small>
-              </article>
-              <article className="skill__details">
-                <h4>ASMx64</h4>
+                <h4>Kubernetes</h4>
                 <small className='text-light'>Beginner</small>
-              </article>
-              <article className="skill__details">
-                <h4>Bash & Shell</h4>
-                <small className='text-light'>Experienced</small>
-              </article>
-              <article className="skill__details">
-                <h4>Makefile</h4>
-                <small className='text-light'>Experienced</small>
               </article>
             </div>
           </div>
