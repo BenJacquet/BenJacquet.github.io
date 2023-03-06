189d14b39be8e5442cae1da423118dd60b4697cc
benjacquet <jabenjam@student.42.fr>
2023-03-06 21:03:43 +0100

    added google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import React from 'react'
 import App from './App'
 import './App.css'
 
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-EE9BPHBYQM";
+ReactGA.initialize(TRACKING_ID);
+
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(<App tab="home" />);
